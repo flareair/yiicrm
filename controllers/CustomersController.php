7f50290593d74df4966ec5788896faa9df5f4d34
flareair <flareair0@gmail.com>
2015-09-23 15:53:59 +0300

    Lot of work with customers controller, added add action, helper private methods to work with active records

--- controllers/CustomersController.php
+++ controllers/CustomersController.php
@@ -3,13 +3,53 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use app\models\customer\Customer;
+use app\models\customer\CustomerRecord;
+use app\models\customer\PhoneRecord;
 
 class CustomersController extends Controller
 {
     public function actionIndex()
     {
         $records = $this->getRecordsAccordingToQuery();
-        $this->render('index', compact('records'));
+        return $this->render('index', compact('records'));
         // return 'action!';
+    }
+
+    public function actionAdd()
+    {
+        $customer = new CustomerRecord;
+        $phone = new PhoneRecord;
+        return $this->render('add', compact('customer', 'phone'));
+    }
+
+    private function store(Customer $customer)
+    {
+        $customerRecord = new CustomerRecord();
+        $customerRecord->name = $customer->name;
+        $customerRecord->birth_date = $customer->birthDate->format('Y-m-d');
+        $customerRecord->notes = $customer->notes;
+        $customerRecord->save();
+
+        foreach ($customer->phones as $phone) {
+            $phoneRecord = new PhoneRecord();
+            $phoneRecord->number = $phone->number;
+            $phoneRecord->customer_id = $customerRecord->id;
+            $phoneRecord->save();
+        }
+    }
+
+    private function makeCustomer(
+        CustomerRecord $customerRecord,
+        PhoneRecord $phoneRecord
+    ) {
+        $name = $customerRecord->name;
+        $birthDate = $customerRecord->birth_date;
+
+        $customer = new Customer($name, $birthDate);
+        $customer->notes = $customerRecord->notes;
+
+        // need to be changed for multiple numbers
+        $customer->phones[] = new Phone($phoneRecord->number);
     }
 }
