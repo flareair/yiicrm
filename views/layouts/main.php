82fea7ceb359342e1073b6065ec62be0924cb391
flareair <flareair0@gmail.com>
2015-09-24 12:16:12 +0300

    Added view for customers and main layout

---
+++ views/layouts/main.php
@@ -0,0 +1,20 @@
+<?php
+use yii\helpers\Html;
+?>
+
+<?php $this->beginPage();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <?= Html::csrfMetaTags();?>
+    <title><?= Html::encode($this->title);?></title>
+    <?php $this->head();?>
+</head>
+<body>
+<?php $this->beginBody();?>
+    <?= $content;?>
+<?php $this->endBody();?>
+</body>
+</html>
+<?php $this->endPage();?>
