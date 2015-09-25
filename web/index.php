13ae5e1878a2a1bf69b69d67cfb1920a8e4dec22
flareair <flareair0@gmail.com>
2015-09-25 12:03:34 +0300

    Added more complex error messages

--- web/index.php
+++ web/index.php
@@ -3,6 +3,11 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
+if (YII_DEBUG === true && YII_ENV === 'dev') {
+    ini_set('display_errors',1);
+    error_reporting(E_ALL);
+}
+
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
